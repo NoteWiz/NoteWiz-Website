0eb9011868bbf0d3522530e211a3b451599b1691
Aaqeb11 <aaqebahmed@gmail.com>
2024-04-03 20:14:25 +0300

    landing page complete

--- notewiz/app/page.tsx
+++ notewiz/app/page.tsx
@@ -277,8 +277,75 @@ export default function Home() {
       <div className="bg-[#FFE57D] w-full h-screen ">
         <div className="bg-[#FFE57D] w-full h-[50%]"></div>
         <div className="bg-[#0C3464] w-full h-[50%]">
-          <footer>
-            <div></div>
+          <footer className="">
+            <div className="flex gap-[350px]">
+              <div className="flex flex-col">
+                <a
+                  href="/"
+                  className="flex items-center space-x-1 ml-[70px] mt-[200px]"
+                >
+                  <div className="h-[30px] w-[30px] rounded-full bg-white"></div>
+                  <span className="self-center text-2xl text-white font-semibold whitespace-nowrap">
+                    NoteWiz
+                  </span>
+                </a>
+                <div className="flex gap-4">
+                  <label className="relative block">
+                    <span className="sr-only">Search</span>
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-2"></span>
+                    <input
+                      className="placeholder:italic placeholder:text-slate-400 block bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm ml-[60px] mt-[20px]"
+                      placeholder="enter your email"
+                      type="email"
+                      name="search"
+                    />
+                  </label>
+                  <div className="flex items-center">
+                    <button className="bg-[#FFE57D] rounded-xl px-10 py-2 mt-4">
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex gap-[180px] mt-[220px]">
+                <div className="flex flex-col ">
+                  <p className="text-3xl text-white font-semibold">
+                    Quick Links
+                  </p>
+                  <div className="pt-4 flex flex-col gap-2">
+                    <a
+                      href="/"
+                      className="text-white underline decoration-solid hover:text-black"
+                    >
+                      Terms and conditions
+                    </a>
+                    <a
+                      href="/"
+                      className="text-white underline decoration-solid hover:text-black"
+                    >
+                      Terms and conditions
+                    </a>
+                  </div>
+                </div>
+                <div className="flex flex-col ">
+                  <p className="text-3xl text-white font-semibold">Contacts</p>
+                  <div className="pt-4 flex flex-col gap-2">
+                    <a
+                      href="/"
+                      className="text-white underline decoration-solid hover:text-black"
+                    >
+                      info@noteWiz.com
+                    </a>
+                    <a
+                      href="/"
+                      className="text-white underline decoration-solid hover:text-black"
+                    >
+                      If you made it this far drop a follow :)
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </footer>
         </div>
       </div>
