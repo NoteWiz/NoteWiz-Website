59c04f9d2aa686825e622e1ccc05712d54c27812
Waqas Faraz <farazwaqas75@gmail.com>
2024-04-04 02:34:09 +0530

    dashboard added

---
+++ notewiz/app/constants/index.js
@@ -0,0 +1,63 @@
+import Home from "@/assets/Home.png";
+import Brain from "@/assets/Brain.png";
+import Layers from "@/assets/Layers.png";
+import Users from "@/assets/Users.png";
+import Comments from "@/assets/Comments.png";
+import GraduationCap from "@/assets/Graduation-cap.png";
+import Ranking from "@/assets/Ranking-star.png";
+import Settings from "@/assets/Settings.png";
+import Bell from "@/assets/Bell.png";
+import PaperPlane from "@/assets/Paper-plane.png";
+
+export const sideBarLinks = [
+  {
+    imgURL: Home,
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: PaperPlane,
+    route: "/chatbot",
+    label: "Chat",
+  },
+  {
+    imgURL: Brain,
+    route: "/mindmaps",
+    label: "Mindmaps",
+  },
+  {
+    imgURL: Layers,
+    route: "/flashcard",
+    label: "Flashcards",
+  },
+  {
+    imgURL: Users,
+    route: "/dashboard",
+    label: "Account",
+  },
+  {
+    imgURL: Comments,
+    route: "/Chatting",
+    label: "Comments",
+  },
+  {
+    imgURL: GraduationCap,
+    route: "/practice",
+    label: "Practice",
+  },
+  {
+    imgURL: Ranking,
+    route: "/leaderboard",
+    label: "Leaderboard",
+  },
+  {
+    imgURL: Settings,
+    route: "/Settings",
+    label: "Settings",
+  },
+  {
+    imgURL: Bell,
+    route: "/Notification",
+    label: "Notification",
+  },
+];
