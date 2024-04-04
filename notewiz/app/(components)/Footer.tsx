b91a7d4185d5758582164a72c4347864aeb00040
Aaqeb11 <aaqebahmed@gmail.com>
2024-04-04 19:55:32 +0300

    fix: footer issues

--- notewiz/app/(components)/Footer.tsx
+++ notewiz/app/(components)/Footer.tsx
@@ -2,41 +2,55 @@ import { FooterInfo } from "./FooterInfo";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex gap-[24vw]">
-      <div className="flex flex-col max-w-[30vw] ">
-        <a href="/" className="flex items-center ml-[5vw] mt-[8vw] gap-3">
-          <div className="h-[30px] w-[30px] rounded-full bg-white flex"></div>
-          <span className="self-center text-2xl text-white font-semibold flex">
-            NoteWiz
-          </span>
-        </a>
-        <div className="flex gap-4">
-          <div className="flex items-center flex-wrap">
-            <input
-              className="placeholder:italic placeholder:text-slate-400 block bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm ml-[60px] mt-[20px]"
-              placeholder="enter your email"
-              type="email"
-              name="search"
-            />
+    <footer className="bg-[#0C3464] ">
+      <div className="flex items-center justify-center px-1 py-16 gap-[38vw] border">
+        <div className="flex flex-col gap-8">
+          <div className="flex gap-2">
+            <div className="h-[30px] w-[30px] bg-white rounded-full"></div>
+            <p className="text-white text-2xl">NoteWiz</p>
           </div>
-
-          <div className="flex items-center">
-            <button className="bg-[#FFE57D] rounded-xl px-10 py-2 mt-4">
-              Submit
-            </button>
+          <div>
+            <div className="flex gap-4">
+              <input
+                type="text"
+                className="rounded-lg text-md bg-white px-4 py-2 shadow-lg"
+                placeholder="name@flowbite.com"
+              />
+              <div className="flex items-end">
+                <button className="bg-[#FFE57D] px-4 py-1 rounded-lg">
+                  Subscribe
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex items-end justify-end w-full pr-8">
-        <div className="flex gap-[180px] items-end justify-end mt-[10vw] ">
-          <FooterInfo
-            info="Terms and conditions"
-            info2="Terms and conditions"
-          />
-          <FooterInfo
-            info="Terms and conditions"
-            info2="Terms and conditions"
-          />
+        <div className="grid grid-cols-2 justify-items-center gap-5 ">
+          <div className="flex flex-col gap-3 max-w-[500px] overflow-hidden text-wrap">
+            <p className="text-white text-xl font-medium">Quick Links</p>
+            <p className="text-white">
+              <a href="/" className="hover:text-black">
+                Terms and Conditions
+              </a>
+            </p>
+            <p className="text-white">
+              <a href="/" className="hover:text-black">
+                Terms and Conditions
+              </a>
+            </p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <p className="text-white text-xl font-medium">Quick Links</p>
+            <p className="text-white">
+              <a href="/" className="hover:text-black">
+                Terms and Conditions
+              </a>
+            </p>
+            <p className="text-white">
+              <a href="/" className="hover:text-black">
+                Terms and Conditions
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </footer>
