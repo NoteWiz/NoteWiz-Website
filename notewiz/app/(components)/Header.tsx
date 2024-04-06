f5f2eebc87afc9aba8d9b9c6d4702e580beab9ac
Aaqeb11 <aaqebahmed@gmail.com>
2024-04-06 22:12:44 +0300

    header gap added

--- notewiz/app/(components)/Header.tsx
+++ notewiz/app/(components)/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const Header = () => {
   return (
     <div className="flex items-center justify-center pt-8 mx-auto">
-      <div className="flex justify-between mx-auto items-center py-[1vw]  bg-[#FFE57D] rounded-3xl fixed mt-16 z-10 md:gap-x-4 gap-x-2 px-3 md:px-4">
+      <div className="flex justify-between mx-auto items-center py-[1vw]  bg-[#FFE57D] rounded-3xl fixed mt-16 z-10 md:gap-x-14 gap-x-2 px-3 md:px-4">
         <a href="/" className="flex items-center md:gap-x-1 gap-x-2">
           <div className="h-[30px] w-[30px] rounded-full bg-black"></div>
           <span className=" text-2xl font-semibold ">NoteWiz</span>
