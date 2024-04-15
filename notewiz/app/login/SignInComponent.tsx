2fce5ed665c60ede2d965cd14ec81be66fe67ee8
Aaqeb11 <aaqebahmed@gmail.com>
2024-04-16 00:38:38 +0300

    backend auth implemented

--- notewiz/app/login/SignInComponent.tsx
+++ notewiz/app/login/SignInComponent.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import Image from 'next/image';
-import GoogleIcon from './google.png';
-import Logo from './notewiz logo.png';
-import Link from 'next/link';
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import GoogleIcon from "./google.png";
+import Logo from "./notewiz logo.png";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const SignInComponent = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const changeHandler = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  let SignIn = async (e: any) => {
+    console.log("Sign in complete", formData);
+    e.preventDefault();
+    let responseData: any = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    // .then((response) => response.json())
+    // .then((data) => (responseData = data));
+    let data = await responseData.json();
+    if (data.success) {
+      console.log("data fetched", data);
+      localStorage.setItem("auth-token", data.token);
+      window.location.replace("/dashboard");
+    } else {
+      alert(data.errors);
+    }
+  };
   return (
-
     <div className="flex flex-col min-h-screen justify-center items-center bg-blue-400">
       {/* Navbar */}
       <nav className="bg-[#FFE57D] py-4 fixed w-full z-10 top-12 rounded-3xl mx-auto max-w-4xl">
@@ -37,44 +70,104 @@ const SignInComponent = () => {
         </div>
       </nav>
 
-
       {/* Sign-in */}
       <section className="flex flex-col items-center justify-center mt-32">
         <div className="bg-white rounded-lg shadow-lg p-12 md:p-16 lg:p-20">
-          <button type="button" className="w-full flex items-center justify-center border border-gray-300 bg-white text-blue-600 font-medium rounded-lg py-3 shadow-md hover:bg-gray-100 mb-4">
-            <Image src={GoogleIcon} alt='Google Icon' width={24} height={24} />
+          <button
+            type="button"
+            className="w-full flex items-center justify-center border border-gray-300 bg-white text-blue-600 font-medium rounded-lg py-3 shadow-md hover:bg-gray-100 mb-4"
+          >
+            <Image src={GoogleIcon} alt="Google Icon" width={24} height={24} />
             <span className="ml-2">Sign in with Google</span>
           </button>
           <div className="flex items-center mb-6">
             <hr className="flex-grow border-gray-300 dark:border-gray-600" />
-            <span className="mx-4 text-sm text-gray-700 dark:text-gray-400">or</span>
+            <span className="mx-4 text-sm text-gray-700 dark:text-gray-400">
+              or
+            </span>
             <hr className="flex-grow border-gray-300 dark:border-gray-600" />
           </div>
           <form className="space-y-4">
             <div>
-              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-              <input type="email" name="email" id="email" className="w-full py-3 px-4 rounded border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={changeHandler}
+                className="w-full py-3 px-4 rounded border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="name@company.com"
+                required
+              />
             </div>
             <div>
-              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input type="password" name="password" id="password" className="w-full py-3 px-4 rounded border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required />
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={formData.password}
+                onChange={changeHandler}
+                className="w-full py-3 px-4 rounded border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="••••••••"
+                required
+              />
             </div>
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <input id="remember" aria-describedby="remember" type="checkbox" className="h-4 w-4 text-primary-500 border-gray-300 rounded focus:ring-primary-500 dark:border-gray-600 dark:text-primary-500 dark:focus:ring-blue-500" required />
-                <label htmlFor="remember" className="ml-2 text-sm text-gray-900 dark:text-white">Remember me</label>
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  className="h-4 w-4 text-primary-500 border-gray-300 rounded focus:ring-primary-500 dark:border-gray-600 dark:text-primary-500 dark:focus:ring-blue-500"
+                />
+                <label
+                  htmlFor="remember"
+                  className="ml-2 text-sm text-gray-900 dark:text-white"
+                >
+                  Remember me
+                </label>
               </div>
-              <Link href="#" className="text-sm text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
+              <Link
+                href="#"
+                className="text-sm text-primary-600 hover:underline dark:text-primary-500"
+              >
+                Forgot password?
+              </Link>
             </div>
-            <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-medium rounded-lg py-3">Sign in</button>
+            <button
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-medium rounded-lg py-3"
+              onClick={SignIn}
+            >
+              Sign in
+            </button>
           </form>
           <div className="mt-4 text-center">
-            <p className="text-sm text-gray-700 dark:text-gray-400">Don't have an account yet? <Link href="/signup" className="text-pink-400 hover:underline font-bold">Sign up</Link></p>
+            <p className="text-sm text-gray-700 dark:text-gray-400">
+              Don't have an account yet?{" "}
+              <Link
+                href="/signup"
+                className="text-pink-400 hover:underline font-bold"
+              >
+                Sign up
+              </Link>
+            </p>
           </div>
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default SignInComponent;
