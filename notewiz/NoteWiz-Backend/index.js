844a8c33254c8c9fc4b384581a3beea6e7715f86
Juwairia Baig <juwairiabaig2003@gmail.com>
2024-04-21 01:01:41 +0530

    shush api key again

--- notewiz/NoteWiz-Backend/index.js
+++ notewiz/NoteWiz-Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const API_KEY = process.env.API_KEY;   uncomment when using
+const API_KEY = process.env.API_KEY; 
 
 app.post('/connections', async (req, res) => {
 
