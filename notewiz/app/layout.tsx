7a538f1c48e3367e28a55da9c7ecce59e6885a23
Aaqeb11 <aaqebahmed@gmail.com>
2024-09-09 11:55:07 +0530

    feat:added google analytics

--- notewiz/app/layout.tsx
+++ notewiz/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/lib/SessionProvider";
 import localFont from "next/font/local"
+import Script from 'next/script'
 
 const dm_sans = DM_Sans({ subsets: ["latin"] });
 
@@ -42,9 +43,28 @@ export default async function RootLayout({
   const session = await getServerSession();
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M3925D8N');
+          `}
+        </Script>
+      </head>
       <SessionProvider session={session}>
-        <body className={`${grotesque.variable} ${dm_sans.className}`} font-sans-serif='true'>{children}</body>
-      </SessionProvider>
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M3925D8N"
+              height="0" 
+              width="0" 
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+          {children}
+        </SessionProvider>
     </html>
   );
 }
