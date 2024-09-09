f6067f007b14679f55537015a53b94ac5601743c
Aaqeb11 <aaqebahmed@gmail.com>
2024-09-09 12:26:23 +0530

    fix:google tag

--- notewiz/app/layout.tsx
+++ notewiz/app/layout.tsx
@@ -50,21 +50,19 @@ export default async function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M3925D8N');
+            })(window,document,'script','dataLayer','GTM-P57L3TL3');
           `}
         </Script>
       </head>
-      <SessionProvider session={session}>
-          <noscript>
-            <iframe 
-              src="https://www.googletagmanager.com/ns.html?id=GTM-M3925D8N"
-              height="0" 
-              width="0" 
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
+      <body className={`${grotesque.variable} ${dm_sans.className}`}>
+        <SessionProvider session={session}>
+        
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P57L3TL3"
+            height="0" width="0" style={{ display:'none',visibility:'hidden'}}></iframe></noscript>
+
           {children}
         </SessionProvider>
+      </body>
     </html>
   );
 }
